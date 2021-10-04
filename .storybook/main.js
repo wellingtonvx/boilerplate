cad399e90bc15ca4e90ea816824179861a160caa
Wellington <wellingtonvx@gmail.com>
2021-10-04 15:24:05 -0300

    finish boilerplate

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
-  ]
+  addons: ['@storybook/addon-essentials']
 }
-
-// /*stories old
-//     "../src/**/*.stories.mdx",
-//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
-// */
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import Main from '.'
+import { Main } from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJs, NextJs e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
